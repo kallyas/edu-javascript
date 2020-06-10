4fc20b2ef7bf0d87d2afb20dfb90aa33154c7e85
Kally <kallyasmedia@gmail.com>
2020-06-10 14:49:10 +0000

    package.json

---
+++ index.js
@@ -0,0 +1,19 @@
+let firstName = "Kallyas";
+let lastName = "Iden";
+
+//log to the console
+console.log(`${firstName} ${lastName}`);
+
+//toLowerCase
+console.log(`${firstName} ${lastName}`.toLowerCase());
+
+//toUpperCase
+console.log(`${firstName} ${lastName}`.toUpperCase());
+
+//split()
+// creates an array from the strings
+console.log(`${firstName} ${lastName}`.split(" ")); //strings array
+console.log(`${firstName} ${lastName}`.split('')); //single character array
+
+
+
