6f1cca3f56b627768b768e5724625d0ceee5aeb4
Kally <kallyasmedia@gmail.com>
2020-06-10 15:28:54 +0000

    objects

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+//strings
 let firstName = "Kallyas";
 let lastName = "Iden";
+
+const fullName = `${firstName} ` + `${lastName}`;
 
 //log to the console
 console.log(`${firstName} ${lastName}`);
@@ -15,5 +18,40 @@ console.log(`${firstName} ${lastName}`.toUpperCase());
 console.log(`${firstName} ${lastName}`.split(" ")); //strings array
 console.log(`${firstName} ${lastName}`.split('')); //single character array
 
+//fullName
+console.log(fullName);
 
+//Numbers
+let num = 7.77;
 
+//round off
+console.log(num.toFixed(1));
+
+//Arrays
+let numbers = [5, 7, 6];
+
+//add/append elemts to the array
+numbers.push(9, 2, 4);
+console.log(numbers);
+
+//pop() removes the last element in an array and returns it
+console.log(numbers.pop());
+
+//iterate through an array forEach()
+numbers.forEach((element) => {
+    console.log(element);
+})
+
+//objects
+let person = {
+    firstName: 'Iden',
+    lastName: 'Kallyas',
+    address: {
+        city: 'Austin',
+        state: 'Texas'
+    },
+    age: 25,
+    cats: ['Milo', 'Tito', 'Achiles']
+};
+
+console.log(person);
